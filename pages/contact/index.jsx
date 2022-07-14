a9f5be89a99ff054df8e9fe312571c07bc728a5c
Justin Pilapil <justin.pilapil@yahoo.com>
2022-07-14 14:07:09 -0700

    add form handling

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,34 +1,69 @@
+import { useState } from "react";
 import styles from "./Contact.module.scss";
 
 const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    message: "",
+    contact: "",
+  });
+
+  const handleNameChange = (e) => {
+    setFormData({ ...formData, name: e.target.value });
+  };
+
+  const handleMessageChange = (e) => {
+    setFormData({ ...formData, message: e.target.value });
+  };
+
+  const handleContactChange = (e) => {
+    setFormData({ ...formData, contact: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
-    <p className={`${styles.formText} is-size-4 my-6`}>
-      What&apos;s up Justin, my name is&nbsp;
-      <input
-        type="text"
-        className={styles.contactInput}
-        id="input-name"
-        aria-describedby="inputName"
-        placeholder="your name"
-      />{" "}
-      and I am trying to contact you because&nbsp;
-      <input
-        type="text"
-        className={styles.contactInput}
-        id="input-text"
-        aria-describedby="inputText"
-        placeholder="your message"
-      />
-      . I can be reached at&nbsp;
-      <input
-        type="email"
-        className={styles.contactInput}
-        id="input-email"
-        aria-describedby="emailHelp"
-        placeholder="your email/phone"
-      />
-      , I am looking forward to chatting with you.
-    </p>
+    <>
+      <p className={`${styles.formText} is-size-4 my-6`}>
+        What&apos;s up Justin, my name is&nbsp;
+        <input
+          type="text"
+          className={styles.contactInput}
+          id="name"
+          aria-describedby="inputName"
+          placeholder="your name"
+          value={formData.name}
+          onChange={handleNameChange}
+        />{" "}
+        and I am trying to contact you because&nbsp;
+        <input
+          type="text"
+          className={styles.contactInput}
+          id="message"
+          aria-describedby="inputText"
+          placeholder="your message"
+          value={formData.message}
+          onChange={handleMessageChange}
+        />
+        . I can be reached at&nbsp;
+        <input
+          type="email"
+          className={styles.contactInput}
+          id="email"
+          aria-describedby="inputEmail"
+          placeholder="your email/phone"
+          value={formData.contact}
+          onChange={handleContactChange}
+        />
+        , I am looking forward to chatting with you.
+      </p>
+      <button className={`${styles.send} button`} onClick={handleSubmit}>
+        SEND IT
+      </button>
+    </>
   );
 };
 
@@ -51,7 +86,6 @@ export default function Contact() {
           .
         </p>
         <ContactForm />
-        <button className={`${styles.send} button`}>SEND IT</button>
       </div>
     </div>
   );
