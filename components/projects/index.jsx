85c8e7ea22e0f06723df3bc2e4f7d452c60fbd5d
Justin Pilapil <justin.pilapil@yahoo.com>
2022-07-21 15:23:21 -0700

    update px to rem

--- components/projects/index.jsx
+++ components/projects/index.jsx
@@ -1,46 +1,12 @@
 import { useState } from "react";
+import { v4 as getUuid } from "uuid";
 import styles from "./Projects.module.scss";
 
 export default function Projects() {
-  const projects = ["test", "test", "test"];
-  const [activeStep, setActiveStep] = useState(0);
-  const maxSteps = projects.length;
-
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
   return (
-    <div id="projects" className="column is-6 ">
-      <h1 className={`${styles.header} has-text-centered-mobile`}>PROJECTS</h1>
-      <div className="columns">
-        <div className="column is-6 has-text-centered-mobile">
-          <div key={projects[activeStep].id} id={styles.temp}></div>
-        </div>
-        <div className={`${styles.desc} column is-6 ml-5-mobile`}>
-          <h2 className={styles.title}>project name</h2>
-          <p>project desc</p>
-
-          <button
-            className={`${styles.backNext} button pl-0`}
-            onClick={handleBack}
-            disabled={activeStep === 0}
-          >
-            BCK
-          </button>
-          <span id={styles.line}></span>
-          <button
-            className={`${styles.backNext} button`}
-            onClick={handleNext}
-            disabled={activeStep === maxSteps - 1}
-          >
-            NXT
-          </button>
-        </div>
-      </div>
-    </div>
+    <section id="projects" className="section is-medium">
+      <h1 className={`${styles.header} has-text-left`}>Dope @#$%</h1>
+      <div className="columns"></div>
+    </section>
   );
 }
