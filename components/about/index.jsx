6de5db40176ff544f9722ae937a17cb10d785a56
Justin Pilapil <justin.pilapil@yahoo.com>
2022-08-04 20:46:14 -0700

    edit about

--- components/about/index.jsx
+++ components/about/index.jsx
@@ -7,17 +7,17 @@ const Bio = () => {
     <div className="column has-text-left">
       <h1 className={styles.header}>About Me</h1>
       <p className="mt-6">
-        Hey, my name is Justin, a software developer with over 3 years of
+        Hey my name is Justin, a software developer with over 3 years of
         professional experience. I consider myself a problem solver with
-        creative visual solutions. I enjoy exploring possibilities and am
+        creative visual solutions. I enjoy exploring possibilities and I'm
         constantly searching for challenges to improve my skills.
       </p>
       <br />
       <p>
         I belive that my job as a developer is more than just creating
         eye-catching, easy to use applications. It is more about closing the gap
-        between the user and their goals while combining business goals with
-        creative solutions. In my free time, I am currently dabbling in the
+        between the user and their goals, achieving this through combining
+        business goals with creative solutions. In my free time, I dabble in the
         world of cryptocurrency, blockchain, and game development.
       </p>
       <br />
